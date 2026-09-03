fc2dd34021a76f67c733aa5ef7d83223571acaa2
AtumCor <jerrymeng007@gmail.com>
2026-09-03 13:24:50 -0400

    added 404 fallback

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+
+  const url = new URL(redirect);
+
+  window.history.replaceState(
+    null,
+    "",
+    url.pathname + url.search + url.hash
+  );
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
